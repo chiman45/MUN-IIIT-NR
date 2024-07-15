26d2218250039ab783c02d487b75f6861388fbec
priyadarshi7 <priyadarshisatyakam3@gmail.com>
2024-07-15 14:37:16 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Home from "./Components/Pages/Home"
+
+//Import Components
+
+export default function App(){
+     return(
+      <div className="main">
+        <Home />
+      </div>
+     )
+}
