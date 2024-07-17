b5eda23a1db224323c3b54a57e20ab2a257370f6
VerShyCoder <anishisbusy@gmail.com>
2024-07-17 21:29:29 +0530

    Add FAQs and ContactUs components

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import React from "react"
 import Home from "./Components/Pages/Home"
 import Diplomacy from "./Components/Pages/Diplomacy/Diplomacy"
 import About from "./Components/Pages/About/About"
+import FAQs from "./Components/Pages/FAQs/FAQs"
+import ContactUs from "./Components/Pages/ContactUs/ContactUs"
 
 //Import Components
 
-export default function App(){
-     return(
-      <div className="main">
-        <Diplomacy/>
-        <About/>
-      </div>
-     )
+export default function App() {
+  return (
+    <div className="main">
+      <Diplomacy />
+      <About />
+      <FAQs />
+      <ContactUs />
+    </div>
+  )
 }
