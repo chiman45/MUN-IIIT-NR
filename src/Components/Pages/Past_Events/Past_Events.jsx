81b231659553f89930d007f953fc702e1274825b
alisha3693 <tripathialisha03@gmail.com>
2024-07-25 18:43:10 +0530

    past events fixed

--- src/Components/Pages/Past_Events/Past_Events.jsx
+++ src/Components/Pages/Past_Events/Past_Events.jsx
@@ -7,11 +7,12 @@ export default function PastEvents(){
             <div className="PastEventscontainer">
             <h1>Past Events</h1>
 <div className="image1">
-            <div className="img1" >
-                </div>
+            <img src={PastEventIMG} className="img1"/ >
             <button className="button">Know more</button>
+                </div>
+            
             </div>
-            </div>
+            
         </div>
     )
 }
