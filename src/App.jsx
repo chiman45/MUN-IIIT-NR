8edc7aecc008fc264992c8aa2c2b1a03788d075e
saitama-on <yashjoshi6787@gmail.com>
2024-07-25 16:43:15 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import About from "./Components/Pages/About/About"
 import FAQs from "./Components/Pages/FAQs/FAQs"
 import ContactUs from "./Components/Pages/ContactUs/ContactUs"
 import PastEvents from "./Components/Pages/Past_Events/Past_Events.jsx"
+import Navbar from "./Components/Navbar/Navbar.jsx"
 //Import Components
 
 export default function App() {
   return (
     <div className="main">
+      
       <Diplomacy />
       <About />
       <PastEvents />
