d5120dfebddd474dfb82dbb11203ce7bae27fde4
priyadarshi7 <priyadarshisatyakam3@gmail.com>
2024-08-17 11:33:24 +0530

    changes

--- src/Components/Pages/Past Events/Events_Grid/Events_Grid.jsx
+++ src/Components/Pages/Past Events/Events_Grid/Events_Grid.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Events_Grid.css'
+import grid_img from '../../../../Assets/Images/Grid/grid.png'
 
 const Events_Grid = () => {
   return (
--- src/Components/Pages/Past Events/PastEvents.jsx
+++ src/Components/Pages/Past Events/PastEvents.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import MainPage from './Main Page/MainPage'
 import Committee from './Committee/Committee'
 import Events_Grid from './Events_Grid/Events_Grid'
+import './PastEvents.css'
 
 const PastEvents = () => {
   return (
-    <div>
+    <div className='past-events-main'>
         <MainPage/>
         <Committee/>
         <Events_Grid/>
