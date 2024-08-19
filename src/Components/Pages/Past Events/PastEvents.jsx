57cf4090e6484ad730b272494356ddbc47d5ce37
alisha3693 <tripathialisha03@gmail.com>
2024-08-19 19:33:29 +0530

    added gallery

--- src/Components/Pages/Past Events/PastEvents.jsx
+++ src/Components/Pages/Past Events/PastEvents.jsx
@@ -4,13 +4,14 @@ import Committee from './Committee/Committee'
 import Events_Grid from './Events_Grid/Events_Grid'
 import './PastEvents.css'
 import Testimonials from './Testimonials/Testimonials'
-
+import Gallery from "./Gallery/Gallery"
 const PastEvents = () => {
   return (
     <div className='past-events-main'>
         <MainPage/>
         <Committee/>
         <Events_Grid/>
+        <Gallery/>
         <Testimonials/>
     </div>
   )
